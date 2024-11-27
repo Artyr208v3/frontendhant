c57a6cb2a2a6624195d9c982b5b70cc440e88b1a
Mikhail <m-kokorin@yandex.ru>
2024-11-27 19:40:36 +0500

    test modal logic

---
+++ src/components/test/LinkButton.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	let {
+		label,
+		url
+	}: {
+		label: string;
+		url: string;
+	} = $props();
+</script>
+
+<a class="link-button" href={url}>{label}</a>
+
+<style>
+	.link-button {
+		background-color: #f2dad0;
+		color: #2c4194;
+		font-size: 24px;
+		text-align: center;
+		margin: 10px;
+		cursor: pointer;
+		padding: 0 30px 0 30px;
+		border: none;
+		text-decoration: none;
+		/* display: block; */
+	}
+</style>
--- src/routes/newGame/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-	import Menu from "../../components/Menu.svelte";
-
-</script>
-
-    <Menu/>
